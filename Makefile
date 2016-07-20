5925e22b478fe85f3e52149bd284a0727314ff42
Marco Guerri <marco.guerri@cern.ch>
2016-07-20 21:47:16 +0200

    Added libcrc library

--- Makefile
+++ Makefile
@@ -1,11 +1,22 @@
 obj-m += xgene_probe.o
-xgene_probe-objs := xgene.o crc32.o
+xgene_probe-objs := xgene.o libcrc/libcrc.a
 
-LIB_DIR = /lib/modules/$(shell uname -r)/build
-CFLAGS_xgene.o := -I$(LIB_DIR)/drivers/net/ethernet/apm/xgene
+LIBCRC_DIR = $(PWD)/libcrc
+KERNEL_HEADERS = /lib/modules/$(shell uname -r)/build
+
+CFLAGS_xgene.o := -I$(KERNEL_HEADERS)/drivers/net/ethernet/apm/xgene
+LDFLAGS_xgene_probe.o += -L$(LIBCRC_DIR)/libcrc
 
 all:
-	make -C $(LIB_DIR) M=$(PWD) modules
+	make -C $(KERNEL_HEADERS) M=$(PWD) modules
+
+$(LIBCRC_DIR)/libcrc.a: 
+	# Not relying on Kbuild, need to define include paths manually via EXTRA_CFLAGS.
+	# isystem is evaluated after -I and before default system include paths
+
+	make -C $(LIBCRC_DIR) libcrc.a \
+	EXTRA_CFLAGS="-D__KERNEL__ -isystem $(KERNEL_HEADERS)/include -isystem $(KERNEL_HEADERS)/arch/$(ARCH)/include"
 
 clean:
-	make -C $(LIB_DIR) M=$(PWD) clean
+	make -C libcrc $@
+	make -C $(KERNEL_HEADERS) M=$(PWD) clean
--- xgene.c
+++ xgene.c
@@ -1,5 +1,6 @@
 #include <linux/module.h>
 #include <linux/version.h>
+#include <linux/types.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/kprobes.h>
@@ -8,7 +9,7 @@
 #include <linux/string.h>
 #include <asm/page.h>
 
-#include "crc.h"
+#include "libcrc/crc.h"
 
 #include "xgene_enet_main.h"
 #include "xgene_enet_hw.h"
@@ -18,17 +19,19 @@
 static int xgene_enet_rx_frame_probe(struct xgene_enet_desc_ring *rx_ring,
                                      struct xgene_enet_raw_desc *raw_desc) 
 {
-        u32 skb_index, datalen;
-        u32 i,j;
-        u8 status;
+        uint32_t skb_index, datalen;
+        uint8_t status;
         struct sk_buff *skb;
         struct xgene_enet_desc_ring *buf_pool;
 
-       
+	crc_t crc;
+	crc_params_t crc_params;
+
         /* Get the buffer pool from the current ring */
         buf_pool = rx_ring->buf_pool;
 
-        /* Get the index of the buffer (skb) to which the NIC DMAed to. Then get the skb itself */
+        /* Get the index of the buffer (skb) to which the NIC DMAed to. 
+         * Then get the skb itself */
         skb_index = GET_VAL(USERINFO, le64_to_cpu(raw_desc->m0));
         skb = buf_pool->rx_skb[skb_index];
 
@@ -36,23 +39,30 @@ static int xgene_enet_rx_frame_probe(struct xgene_enet_desc_ring *rx_ring,
                   GET_VAL(LERR, le64_to_cpu(raw_desc->m0));
 
         if (unlikely(status > 2)) {
-                printk("raw descriptor reported and error\n");
+                printk("raw descriptor reported an error\n");
                 goto out;
         }
 
         /* Get buffer lenght from raw_descriptor */
         datalen = GET_VAL(BUFDATALEN, le64_to_cpu(raw_desc->m1));
         datalen = (datalen & DATALEN_MASK);
-        
+
         /* Verifying FCS only for non-paged sk_buff and returning the physical address
          * of the buffer */
         if(!skb_is_nonlinear(skb)) 
         {
-            u32 crc32_compare = crc32_ethernet(crc32_fast(skb->data, datalen-4));
-            if(crc32_compare != *((u32*)(skb->data+datalen-4))){
+            crc_params.type = CRC32;
+	    crc_params.poly.poly_crc32 = 0x04C11DB7;
+	    crc_params.crc_init.crc32 = 0xFFFFFFFF;
+            crc_params.flags = CRC_INPUT_REVERSAL |
+			       CRC_OUTPUT_REVERSAL |
+			       CRC_OUTPUT_INVERSION;
+	
+            crc = crc_fast(&crc_params, (uint8_t*)skb->data, datalen-4);
+            if(crc.crc32 != *((uint32_t*)(skb->data+datalen-4))){
                 printk("Calculated CRC is %x,  CRC in frame is %x, phys: %016llx, %016llx\n", 
-               crc32_compare, 
-               *((u32*)(skb->data+datalen-4)), 
+               	 	crc.crc32, 
+	               *((uint32_t*)(skb->data+datalen-4)), 
                        virt_to_phys(skb->data), 
                        __pa(skb->data));
             }
